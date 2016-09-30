5fb1235a697beb6a1457d0af4b7e61991c38127f
Maxwell Watson <Maxwell Watson>
2016-09-29 21:53:47 -0600

    Added instance variables and data structures for basic board structure. Recreated IntBoardTests as a JU test unit.

--- src/experiment/IntBoard.java
+++ src/experiment/IntBoard.java
@@ -1,5 +1,31 @@
 package experiment;
 
-public class IntBoard {
+import java.util.Map;
+import java.util.Set;
 
+public class IntBoard {
+	private BoardCell[][] boardGrid;
+	private Map<BoardCell, Set<BoardCell>> adjencyList;
+	private Set<BoardCell> visitedList;
+	private Set<BoardCell> targetList;
+	
+	public IntBoard() {
+		
+	}
+	
+	public void calcAdjacencies() {
+		return;
+	}
+	
+	public void calcTargets(BoardCell startCell, int pathLength) {
+		return;
+	}
+	
+	public Set<BoardCell> getTargets() {
+		return null;
+	}
+	
+	public Set<BoardCell>getAdjList() {
+		return null;
+	}
 }
