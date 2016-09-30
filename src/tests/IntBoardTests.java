0c3f75cd94363432c64a982f7a92530df92377f9
Bane Sullivan <chrsulli@mines.edu>
2016-09-30 14:26:12 -0600

    created/updated structure for all adjancency tests

--- src/tests/IntBoardTests.java
+++ src/tests/IntBoardTests.java
@@ -8,15 +8,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class IntBoardTests {
-	/*
-	 * Corners are numbered as:
-	 * 		0___________1
-	 * 		|			|
-	 * 		|			|
-	 * 		|			|
-	 * 		|			|
-	 * 		3___________2
-	 */
+	
 	private IntBoard board;
 	
 	
@@ -29,7 +21,7 @@ public class IntBoardTests {
 	
 	/*
 	 * Test adjacencies for top left corner
-	 *
+	 * top left corner (i.e., location [0][0])
 	 */
 	@Test
 	public void testAdjacency0() {
@@ -39,16 +31,50 @@ public class IntBoardTests {
 		assertTrue(testList.contains(board.getCell(0, 1)));
 		assertEquals(2, testList.size());
 	}
+	
+	
+	/*
+	 * Test adjacencies for bottom right corner
+	 * bottom right corner (i.e., location [3][3])
+	 */
 	@Test
 	public void testAdjacency1() {
 		fail("Not yet implemented");
 	}
+	
+	/*
+	 * Test adjacencies for right edge
+	 * a right edge (e.g., location [1][3])
+	 */
 	@Test
 	public void testAdjacency2() {
 		fail("Not yet implemented");
 	}
+	
+	/*
+	 * Test adjacencies for left edge
+	 * a left edge (e.g., location [3][0])
+	 */
 	@Test
 	public void testAdjacency3() {
+		fail("Not yet implemented");
+	}
+	
+	/*
+	 * Test adjacencies for left edge second column, middle of grid 
+	 * (e.g., location [1][1])
+	 */
+	@Test
+	public void testAdjacency4() {
+		fail("Not yet implemented");
+	}
+	
+	/*
+	 * Test adjacencies for second from last column, middle of grid
+	 * (e.g., location [2][2]).
+	 */
+	@Test
+	public void testAdjacency5() {
 		fail("Not yet implemented");
 	}
 	
