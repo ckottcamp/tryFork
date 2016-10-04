66e34097c01cc286a4078fdad57b545541d7c6ec
Maxwell Watson <mawatson@adit.mines.edu>
2016-10-04 11:57:46 -0600

    Updated BoardCell class with initial variable and methods. Created enum type DoorDirection and created custom exception to deal with bad configurations.

--- src/clueGame/BoardCell.java
+++ src/clueGame/BoardCell.java
@@ -3,10 +3,23 @@ package clueGame;
 public class BoardCell {
 	private int row;
 	private int col;
+	private char initial;
 	// TODO: we are going to need to add some sort of cell type variable
 	
 	public BoardCell(int y, int x) {
 		row = y;
 		col = x;
+	}
+	
+	public boolean isWalkway() {
+		return false;
+	}
+	
+	public boolean isRoom() {
+		return false;
+	}
+	
+	public boolean isDoorway() {
+		return false;
 	}
 }
